24e27982ae1e17cfe03aec6542d2a0cb0a9068f8
atarunox <ffxi.nox@hotmail.com>
2025-05-14 00:25:42 -0700

    Add files via upload

---
+++ gear.js
@@ -0,0 +1,11 @@
+export const gearList = [
+  { id: "sheriff_hat", name: "Sheriff's Hat", slot: "Head", effects: { Cunning: 1 } },
+  { id: "scout_vest", name: "Scout Vest", slot: "Torso", effects: { Strength: 1 } },
+  { id: "gunslinger_gloves", name: "Gunslinger Gloves", slot: "Hands", effects: { Combat: 1 } },
+  { id: "cowboy_boots", name: "Cowboy Boots", slot: "Feet", effects: { Agility: 1 } },
+  { id: "bandana_fortitude", name: "Bandana of Fortitude", slot: "Face", effects: { Willpower: 1 } },
+  { id: "spiked_pauldrons", name: "Spiked Pauldrons", slot: "Shoulders", effects: { Defense: 1 } },
+  { id: "trenchcoat_grit", name: "Trenchcoat of Grit", slot: "Coat", effects: { Grit: 1 } },
+  { id: "duster_pants", name: "Hardened Duster Pants", slot: "Pants", effects: { Lore: 1 } },
+  { id: "alchemist_gauntlets", name: "Alchemist’s Gauntlets", slot: "Gloves", effects: { Luck: 1 } }
+];
