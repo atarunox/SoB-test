f8fab2400d6e94d2a77984fe5374156cfb926c26
atarunox <ffxi.nox@hotmail.com>
2025-05-14 15:59:46 -0700

    Delete gear.js

--- gear.js
+++
@@ -1,5 +0,0 @@
-export const gearList = [
-  { id: "sheriff_hat", name: "Sheriff's Hat", slot: "Head", effects: { Cunning: 1 } },
-  { id: "scout_vest", name: "Scout Vest", slot: "Torso", effects: { Strength: 1 } },
-  { id: "cowboy_boots", name: "Cowboy Boots", slot: "Feet", effects: { Agility: 1 } }
-];
